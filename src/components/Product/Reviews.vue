45f2e1109a506358f93408c996506bdb8ff66653
Andrew Morgan <andrew.morgan@mongodb.com>
2019-09-18 13:35:27 -0400

    Added stars to each review element

--- src/components/Product/Reviews.vue
+++ src/components/Product/Reviews.vue
@@ -5,8 +5,52 @@
       <ul id="review-list">
         <li
           v-for="review in reviews"
-          v-bind:key="review">
-            <div class="box"> {{ review.review }}</div>
+          v-bind:key="review.review">
+            <div class="box">
+              {{ review.review }} <br/><br/>
+                <div class="level-left">
+                  <a class="level-item" aria-label="reply">
+                    <span v-if="review.score >= 1" class="icon is-small has-text-warning">
+                      <i class="fas fa-star" aria-hidden="true"></i>
+                    </span>
+                    <span v-else class="icon is-small has-text-warning">
+                      <i class="far fa-star" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="reply">
+                    <span v-if="review.score >= 2" class="icon is-small has-text-warning">
+                      <i class="fas fa-star" aria-hidden="true"></i>
+                    </span>
+                    <span v-else class="icon is-small has-text-warning">
+                      <i class="far fa-star" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="reply">
+                    <span v-if="review.score >= 3" class="icon is-small has-text-warning">
+                      <i class="fas fa-star" aria-hidden="true"></i>
+                    </span>
+                    <span v-else class="icon is-small has-text-warning">
+                      <i class="far fa-star" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="reply">
+                    <span v-if="review.score >= 4" class="icon is-small has-text-warning">
+                      <i class="fas fa-star" aria-hidden="true"></i>
+                    </span>
+                    <span v-else class="icon is-small has-text-warning">
+                      <i class="far fa-star" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                  <a class="level-item" aria-label="reply">
+                    <span v-if="review.score >= 5" class="icon is-small has-text-warning">
+                      <i class="fas fa-star" aria-hidden="true"></i>
+                    </span>
+                    <span v-else class="icon is-small has-text-warning">
+                      <i class="far fa-star" aria-hidden="true"></i>
+                    </span>
+                  </a>
+                </div>
+            </div>
         </li>
       </ul>
     </section>
