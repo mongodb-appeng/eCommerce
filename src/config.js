c821dcec0f4766b39ee1cf29413a8fb212b49b45
Andrew Morgan <andrew.morgan@mongodb.com>
2019-09-23 23:42:15 +0100

    Add config file

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+    appName: 'mongodb-ecommerce',
+    appId: 'ecommerce-iukkg',
+    database: 'ecommerce',
+    aws: {
+        serviceName: 'AWS2',
+        bucket: 'ecommerce-mongodb-mugshots',
+        productBucket: 'ecommerce-mongodb-products'
+    }
+}
+
+module.exports = config;
