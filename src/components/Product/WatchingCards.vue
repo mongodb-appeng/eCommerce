19cbf8951d82e309cd1312cc443cb0bbc1fc4f80
Andrew Morgan <andrew.morgan@mongodb.com>
2019-10-21 16:10:59 +0100

    Added file back

---
+++ src/components/Product/WatchingCards.vue
@@ -0,0 +1,54 @@
+<template>
+    <div> 
+      <section class="section" id="watching-cards">
+        <div v-if="customer.waitingOnProducts.length > 0">
+            <ul id="watch-list">
+                <li
+                    v-for="productID in customer.waitingOnProducts"
+                    v-bind:key="productID">
+                        <WatchingCard v-bind:productID="productID">
+                        </WatchingCard>
+                </li>
+            </ul>
+        </div>
+        <div v-else>
+            <h3 class="title is-4">Not currently watching any products</h3>
+        </div>
+      </section>
+      <br>
+    </div>
+</template>
+
+<script>
+import WatchingCard from "./WatchingCard.vue"
+import { 
+    mapState
+    } from 'vuex';
+
+export default {
+    name: "WatchingCards",
+    components: {
+        WatchingCard
+    },
+    data() {
+        return {
+          status: null
+        }
+    },
+    computed: {
+      ...mapState([
+        'customer'
+      ])
+    }
+}
+</script>
+
+<style scoped>
+  #product-cards {
+    /* background-color: blanchedalmond; */
+    max-height: 10%;
+    display: flex;
+    overflow-y: auto;
+    flex-direction: column;
+  }
+</style>
